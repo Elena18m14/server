a1468f7f2d8087ead5ba3b170cd49c1ff562cdf2
Your Name <user@example.com>
2019-06-23 17:49:48 +0300

    received audio (bytes)

--- src/main/java/Utils/Util.java
+++ src/main/java/Utils/Util.java
@@ -2,6 +2,8 @@ package Utils;
 
 import io.undertow.server.HttpServerExchange;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.Deque;
 import java.util.Map;
 
@@ -9,6 +11,18 @@ public class Util {
     public static String getQueryParametrs(HttpServerExchange exchange, String key) {
         Map<String, Deque<String>> queryParametrs = exchange.getQueryParameters();
         Deque<String> paramsForKey = queryParametrs.get(key);
+
+        ByteArrayOutputStream r = new ByteArrayOutputStream();
+       // AudioInputStream a = new AudioInputStream(r, AudioFormat.Encoding.PCM_FLOAT, 10);
+        exchange.getRequestReceiver().receiveFullBytes((ex, data) -> {
+            try {
+                r.write(data);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+
         if (paramsForKey != null && paramsForKey.size() > 0) {
             return paramsForKey.getFirst();
         } else {
