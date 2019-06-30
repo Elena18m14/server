8c5c585e9e9ace3816e09a46ca41a62175011291
Your Name <user@example.com>
2019-06-30 12:11:43 +0300

    create AudioStream

--- src/main/java/Utils/Util.java
+++ src/main/java/Utils/Util.java
@@ -2,8 +2,13 @@ package Utils;
 
 import io.undertow.server.HttpServerExchange;
 
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Deque;
 import java.util.Map;
 
@@ -12,17 +17,20 @@ public class Util {
         Map<String, Deque<String>> queryParametrs = exchange.getQueryParameters();
         Deque<String> paramsForKey = queryParametrs.get(key);
 
-        ByteArrayOutputStream r = new ByteArrayOutputStream();
-       // AudioInputStream a = new AudioInputStream(r, AudioFormat.Encoding.PCM_FLOAT, 10);
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
         exchange.getRequestReceiver().receiveFullBytes((ex, data) -> {
             try {
-                r.write(data);
+                byteArrayOutputStream.write(data);
             } catch (IOException e) {
                 e.printStackTrace();
             }
         });
 
-
+        ByteArrayInputStream inStream = new ByteArrayInputStream( byteArrayOutputStream.toByteArray() );
+        AudioFormat format = new AudioFormat(16*1000, 16, 1, true, false);
+        //AudioSystem.getAudioFileFormat(r);
+        AudioInputStream ais  = new AudioInputStream( inStream , format, byteArrayOutputStream.size());
         if (paramsForKey != null && paramsForKey.size() > 0) {
             return paramsForKey.getFirst();
         } else {
